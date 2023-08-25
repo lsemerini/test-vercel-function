3d60ae8f057c8a57641c77d0ea7fd148bc2ba23c
Luciano Semerini <lsemerini@globalvision.co>
2023-08-25 10:04:20 -0400

    test

--- api/index.js
+++ api/index.js
@@ -7,12 +7,12 @@ app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.status(200).end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
 app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+  res.status(200).end(`Item: ${slug}`);
 });
 
 app.get('/', (req, res) => {
